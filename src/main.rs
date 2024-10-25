7fb1455df04d7dbd105e44b03499c9265bb6aa6c
Yoav Orot <orot.yoav@gmail.com>
2024-10-24 20:51:51 -0400

    Add progress bar

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ use std::{fs, path::PathBuf};
 
 use anyhow::Result;
 use clap::{Parser, Subcommand};
+use console::{Style, Term};
 use crossref::CrossRef;
 use csv::CsvParser;
+use indicatif::{ProgressBar, ProgressStyle};
 use record::RecordParser;
 
 #[derive(Parser)]
@@ -34,15 +36,51 @@ fn main() -> Result<()> {
 
     fs::create_dir_all(&args.output)?;
 
-    for res in parser.parse()? {
+    let results = parser.parse()?;
+    let pb = ProgressBar::new(results.len() as u64);
+    pb.set_style(
+        ProgressStyle::with_template(if Term::stdout().size().1 > 80 {
+            "{prefix:>12.cyan.bold} [{bar:57}] {pos}/{len} {wide_msg}"
+        } else {
+            "{prefix:>12.cyan.bold} [{bar:57}] {pos}/{len}"
+        })
+        .unwrap()
+        .progress_chars("=> "),
+    );
+    pb.set_prefix("Downloading");
+
+    let green_bold = Style::new().green().bold();
+    let red_bold = Style::new().red().bold();
+
+    for res in results {
+        pb.println(format!(
+            "{:>12} {}: {}",
+            green_bold.apply_to("Searching"),
+            res.author,
+            res.title
+        ));
+
         if let Ok(work) = cross_ref.query_work(&res.author, &res.title) {
             let bibtex_path = args.output.join(sanitize_filename::sanitize(format!(
                 "{}_{}.bib",
                 res.author, res.title
             )));
+            pb.set_message(format!("DOI: {}", work.doi));
             cross_ref.download_work_bibtex(&work.doi, bibtex_path)?;
-        };
+        } else {
+            pb.set_message("Not found");
+            pb.println(format!(
+                "{:>12} {}: {}",
+                red_bold.apply_to("Not found"),
+                res.author,
+                res.title
+            ));
+        }
+
+        pb.inc(1);
     }
+
+    pb.finish_and_clear();
 
     Ok(())
 }
